9277c6356fd7ee9fd58824d37ed28f8144be71d9
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-27 15:59:50 +0530

    final code

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-# def factorial(number):
-#     i = 1
-#     while number > 0:
-#         i = i*number
-#         number = number - 1
-#     return i
+def factorial(number):
+    i = 1
+    while number > 0:
+        i = i*number
+        number = number - 1
+    return i
 
-# def palindrome(value):
-#     i = 0
-#     length = len(value)
-#     inpu = list(value)
-#     output = []
-#     while length > 0:
-#         i = i - 1
-#         length = length - 1
-#         output.append(value[i])
-#     return output == inpu
+def palindrome(value):
+    i = 0
+    length = len(value)
+    inpu = list(value)
+    output = []
+    while length > 0:
+        i = i - 1
+        length = length - 1
+        output.append(value[i])
+    return output == inpu
 
-# def fibonacci(converted_number):
-#     x = [0,1,]     
-#     output = []
-#     if converted_number == 1:
-#         return [0]
-#     elif converted_number == 2:
-#         return [0,1]    
-#     else:
-#         while converted_number > 2:
-#             output = x[-1] + x[-2]
-#             x.append(output)
-#             converted_number = converted_number - 1
-#         return x
+def fibonacci(converted_number):
+    x = [0,1,]     
+    output = []
+    if converted_number == 1:
+        return [0]
+    elif converted_number == 2:
+        return [0,1]    
+    else:
+        while converted_number > 2:
+            output = x[-1] + x[-2]
+            x.append(output)
+            converted_number = converted_number - 1
+        return x
      
 def main():
     try:
@@ -51,22 +51,22 @@ def main():
     finally:
         print("--*--"*12)
 
-# def getuserinput(input_type:str):
-#     if input_type == "main":
-#         reqd = input("choose mode 1 = factorial,2 = fibonacci , 3 = palindrome check ")
-#         con_reqd = int(reqd)
-#         if con_reqd not in [1,2,3]:
-#             raise ValueError("Please enter either 1 or 2 or 3")
-#     elif input_type == "positive_integer":
-#         reqd = input("Please enter positive integer ")
-#         con_reqd = int(reqd)
-#         if con_reqd < 0:
-#             raise ValueError("Please enter positive integer")           
-#     elif input_type == "string":
-#         con_reqd = input("Please enter a string ") 
-#     else:
-#         raise RuntimeError("Please enter valid input type one of -[main, positive_integer, string]")  
-#     return con_reqd, input_type
+def getuserinput(input_type:str):
+    if input_type == "main":
+        reqd = input("choose mode 1 = factorial,2 = fibonacci , 3 = palindrome check ")
+        con_reqd = int(reqd)
+        if con_reqd not in [1,2,3]:
+            raise ValueError("Please enter either 1 or 2 or 3")
+    elif input_type == "positive_integer":
+        reqd = input("Please enter positive integer ")
+        con_reqd = int(reqd)
+        if con_reqd < 0:
+            raise ValueError("Please enter positive integer")           
+    elif input_type == "string":
+        con_reqd = input("Please enter a string ") 
+    else:
+        raise RuntimeError("Please enter valid input type one of -[main, positive_integer, string]")  
+    return con_reqd, input_type
 
 
 if __name__ == "__main__":
